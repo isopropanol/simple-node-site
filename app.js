ce0f4001de817508b7ae31a5e1d3035dc431771c
Paul Ammann <meanmrclean@gmail.com>
2013-05-04 17:46:54 -0400

    app.js written with ;

--- app.js
+++ app.js
@@ -7,6 +7,14 @@ var app =express();
 funciton compile(str,path){
 	return stylus(str)
 		.set('filename',path)
-		.use(nib())
+		.use(nib());
 }
-app.set('views',)
+app.set('views',__dirname+'/views');
+app.set('view engine','jade');
+app.use(express.logger('dev'));
+app.use(stylus.middleware(
+	{
+	src: __dirname+'/public'
+	, compile:complile
+}));
+app.use(express.static(__dirname+'/public'));
