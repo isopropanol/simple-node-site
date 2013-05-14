0fbb816572c6cee47eba151793a223e8399d82a3
Paul Ammann <meanmrclean@gmail.com>
2013-05-14 16:13:45 -0400

    finally got the post request t to work

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var express = require('express')
 	, stylus = require('stylus')
 	, nib = require('nib');
 var app =express();
+app.use(express.bodyParser());
 function compile(str,path){
 	return stylus(str)
 		.set('filename',path)
@@ -17,6 +18,7 @@ app.use(stylus.middleware(
 	, compile:compile
 }));
 app.use(express.static(__dirname+'/public'));
+
 app.get('/',function(req,res){
 	res.render('index',
 		{title:"Hoos Working Where"}		)
@@ -25,10 +27,12 @@ app.get('/selectjob',function(req,res){
 	res.render('selectjob',
 		{title:"What's your plan"}		)
 })
+
 app.post('/selectjob/detailsubmission',function(req,res){
 	//var name = req.body.place;
 		//city = req.body.city,
 		//country= req.body.country;
-	console.log(" in ");
+	console.log(" in "+req.body.place);
+	res.redirect("/");
 });
 app.listen(3000);
